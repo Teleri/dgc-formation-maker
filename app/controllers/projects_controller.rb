1bbea80625b7140d22052f3891678809a38773b2
Michael Knott <knottweb@gmail.com>
2020-07-23 01:56:06 +0100

    index page styled, cards created, new card action working

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -13,7 +13,7 @@ class ProjectsController < ApplicationController
   end
 
   def create
-    @project = Project.new(params[:project])
+    @project = Project.new(project_params)
     if @project.save
       @project.save!
       redirect_to @project
